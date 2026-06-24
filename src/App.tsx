2acab56947a8fc3222c4838508d62c7d9ed5cdac
devb.e515ea <mranhbau@gmail.com>
2026-06-24 21:08:20 +0700

    setup: init shadcn ui to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="text-xl font-bold text-red-500">Hello DevB</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
